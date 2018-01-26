7739ffbc07cdfa759f18be05daad631aba02f968
alexnov <cementmeister@gmail.com>
2018-01-26 16:11:12 +0300

    Some cleaning.

--- WellCem/src/wellcem/MainProg.java
+++ WellCem/src/wellcem/MainProg.java
@@ -1,9 +1,27 @@
 package wellcem;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class MainProg {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		try {
+			// устанавливаем LookAndFeel
+			// UIManager.setLookAndFeel("com.seaglasslookandfeel.SeaGlassLookAndFeel");
+			// UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+			// UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+			UIManager
+					.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (UnsupportedLookAndFeelException e) {
+			System.err
+					.println("Can't use the specified look and feel on this platform.");
+		} catch (Exception e) {
+			System.err
+					.println("Couldn't get specified look and feel, for some reason.");
+		}
 
 	}
 
---
+++ WellCem/src/wellcem/MainWin.java
@@ -0,0 +1,12 @@
+package wellcem;
+
+import javax.swing.JFrame;
+
+public class MainWin extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
